06be6e4e3c4b1089304664d03e82739eaf3931b7
Joe Addlesee <joeaddlesee@gmail.com>
2026-08-25 21:58:24 +0100

    SQL file added, and data validation added to Python file

--- csvRead.py
+++ csvRead.py
@@ -3,5 +3,21 @@ import pandas
 pandas.options.display.max_rows = None
 
 customerData = pandas.read_csv('D:\\SQL-Python-Project\\JAddleseeSQL-Python-Project\\mockdata\\Customers.csv')
+#bringing in customer data from csv file
 
-print(customerData.to_string())
+membershipData = pandas.read_csv('D:\\SQL-Python-Project\\JAddleseeSQL-Python-Project\\mockdata\\Membership_Values.csv')
+#bringing in membership data from csv file
+
+if customerData["member_id"].duplicated().any():
+    raise ValueError("Duplicate membership IDs")
+#validating that there are no dupes
+
+if (customerData["age"] < 0).any():
+    raise ValueError("Invalid age values")
+#validating that there are no negative ages
+
+
+print(customerData.dtypes)
+
+
+
