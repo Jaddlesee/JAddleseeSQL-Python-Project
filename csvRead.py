d9dceee0cca181dbddfe135212db101641571547
Joe Addlesee <joeaddlesee@gmail.com>
2026-08-26 11:57:55 +0100

    python to sql pipeline started and functional

--- csvRead.py
+++ csvRead.py
@@ -1,4 +1,9 @@
 import pandas
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 pandas.options.display.max_rows = None
 
@@ -16,8 +21,36 @@ if (customerData["age"] < 0).any():
     raise ValueError("Invalid age values")
 #validating that there are no negative ages
 
+customerData["member_since"] = pandas.to_datetime(customerData["member_since"], format='%Y/%m/%d')
+#python recognises the date format as a string so this manually converts it to a date format
 
-print(customerData.dtypes)
+# print(customerData.dtypes)
+# type check
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=os.getenv("MYSQL_PASSWORD"),
+    database="membAnalysis"
+)
+
+print("Successfully connected to MySQL!")
+
+SQLcursor = connection.cursor();
+SQL = """INSERT INTO members (member_id, first_name, last_name, age, email, member_since, membership_status, membership_level) 
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+
+print("Rows in dataframe:", len(customerData))
+
+values = [];
+
+for _, row in customerData.iterrows(): 
+   values.append((int(row['member_id']), row['first_name'], row['last_name'], int(row['age']), row['email'], row['member_since'].date(), row['membership_status'], row['membership_level']))
+
+SQLcursor.executemany(SQL, values)
+print("Rows processed:", SQLcursor.rowcount)
 
 
+connection.commit()
 
+print("Data committed")
--- membAnalysis.sql
+++ membAnalysis.sql
@@ -1,13 +1,14 @@
-create DATABASE membAnalysis;
-
+CREATE DATABASE IF NOT exists membAnalysis ;
 USE membAnalysis;
-CREATE TABLE members (
+
+CREATE TABLE IF NOT exists members (
     member_id INT PRIMARY KEY,
     first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    age INT,
+    last_name VARCHAR(100) ,
+    age INT NOT NULL,
     email VARCHAR(255),
     member_since DATE,
     membership_status VARCHAR(20),
     membership_level VARCHAR(20)
 );
+SELECT COUNT(*) FROM members;
